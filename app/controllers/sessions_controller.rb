821e9618a7aa09d539ba7b7f2ca49d8e5c80fd66
Kevin Powell <kevin.m.powell04@gmail.com>
2013-11-13 21:47:11 -0700

    menu changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,8 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_or_create_by_auth(request.env["omniauth.auth"])
     session[:user_id] = @user.id
-    if @user.orders.where(:status => "incomplete").to_a.first.order_items
-      redirect_to cart_path, notice: "Logged in as #{@user.name}"
-    else
-      redirect_to categories_path, notice: "Logged in as #{@user.name}"
-    end
+    redirect_to cart_path, notice: "Logged in as #{@user.name}"
+  
 
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,21 +15,21 @@ categories = Category.create([
 
 entrees = Category.where(:name => 'Entrees').to_a.first
 entrees.items.create([
-  { name: 'Pulled Pork Sandy', description: "Not a Sloppy Joe", price: 7, image_url: "pp.jpg"}, 
-  { name: 'Beef Brisket', description: "Dry rubbed and aged to perfection", price: 8},
-  { name: 'Slab-o-Ribs', description: "Finger lick'n good", price: 9} 
+  { name: 'Pulled Pork Sandy', description: "Not a Sloppy Joe", price: 7, image_url: "20.jpg"}, 
+  { name: 'Beef Brisket', description: "Dry rubbed and aged to perfection", price: 8, image_url: "15.jpg"},
+  { name: 'Slab-o-Ribs', description: "Finger lick'n good", price: 9, image_url: "18.jpg"} 
   ])
 
 sides = Category.where(:name => 'Side Dishes').to_a.first
 sides.items.create([
-  { name: 'Mac and Cheese', description: "Soooooo cheesy", price: 3}, 
-  { name: 'Baked Beans', description: "Better than Bush's", price: 3},
-  { name: 'Cornbread', description: "Made with real corn", price: 3}
+  { name: 'Mac and Cheese', description: "Soooooo cheesy", price: 3, image_url: "17.gif"}, 
+  { name: 'Baked Beans', description: "Better than Bush's", price: 3, image_url: "19.jpg"},
+  { name: 'Cornbread', description: "Made with real corn", price: 3, image_url: "14.jpg"}
   ])
 
 drinks = Category.where(:name => 'Delightful Drinks').to_a.first
 drinks.items.create([
-  { name: 'Iced Tea', description: "Southern Style Sweet Tea", price: 2}, 
-  { name: 'Naragansett Beer', description: "Borderline Disgusting, but it works", price: 4},
-  { name: 'Pop', description: "It's not soda", price: 2}
+  { name: 'Iced Tea', description: "Southern Style Sweet Tea", price: 2, image_url: "13.jpg"}, 
+  { name: 'Naragansett Beer', description: "Borderline Disgusting, but it works", price: 4, image_url: "16.jpg"},
+  { name: 'Pop', description: "It's not soda", price: 2, image_url: "21.jpg"}
   ])
