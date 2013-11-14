6b80870f303e01514e715befac9144ac644b653b
Bryana Knight <brknig11@gmail.com>
2013-11-14 16:13:40 -0700

    update login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,10 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_or_create_by_auth(request.env["omniauth.auth"])
     session[:user_id] = @user.id
+      if session[:order_id]
+        order = Order.find(session[:order_id])
+        order.update(:user_id => @user.id)
+      end
     redirect_to cart_path, notice: "Logged in as #{@user.name}"
   end
 
