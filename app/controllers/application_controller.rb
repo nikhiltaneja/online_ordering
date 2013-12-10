360949dcf78249b189e559add62326e6c8716dcf
Nikhil Taneja <nikhil.a.taneja@gmail.com>
2013-12-10 15:55:56 -0700

    Add viewable_restaurants helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-
-private
+  def viewable_restaurants
+    Restaurant.where(display:true)
+  end
 
 end
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,6 @@
 class RestaurantsController < ApplicationController
   def index
-    @restaurants = Restaurant.all
+    @restaurants = viewable_restaurants
   end
 
   def show
