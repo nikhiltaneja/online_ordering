ea31721bca438831ed0a59462fd9197e7e7b99cb
QuillyT <QuillyT@gmail.com>
2013-12-10 20:57:04 -0700

    Validate uniqueness of restaurant name.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,7 @@ class Restaurant < ActiveRecord::Base
   has_many :roles
   has_many :users, through: :roles
   validates :name, presence: true
+  validates_uniqueness_of :name
   validates :description, presence: true
   validates :status, inclusion: { in: ["approved", "pending", "rejected"] } 
   after_create :create_slug
