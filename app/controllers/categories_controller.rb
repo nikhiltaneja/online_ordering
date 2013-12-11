814702ae36aae782176245a9a3d082dc2ffa7150
QuillyT <QuillyT@gmail.com>
2013-12-11 16:43:36 -0700

    Make category controller pass actual user objects to the view.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,8 +2,12 @@ class CategoriesController < ApplicationController
   def index
     @restaurant = Restaurant.find_by(slug: params[:slug])
     roles = Role.where(restaurant: @restaurant)
-    @admins = roles.where(level: "admin")
-    @stockers = roles.where(level: "stocker")
+    @admins = roles.where(level: "admin").map do |role|
+      role.user
+    end
+    @stockers = roles.where(level: "stocker").map do |role|
+      role.user
+    end
     @categories = Category.all
   end
 end
