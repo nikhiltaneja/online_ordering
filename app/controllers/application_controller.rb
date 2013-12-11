f44353f1c10433ab000c4e7115f4a95db1a30403
Nikhil Taneja <nikhil.a.taneja@gmail.com>
2013-12-10 18:49:51 -0700

    Fix restaurant index view for platform admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
 
   def viewable_restaurants
-    return Restaurant.all if current_user && current_user.platform_admin
+    return Restaurant.all if platform_admin?
     
     Restaurant.where(display:true)
   end
+
+  def platform_admin?
+    current_user && current_user.platform_admin
+  end
+
+  helper_method :platform_admin?
 
 end
