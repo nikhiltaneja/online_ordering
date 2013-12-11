137614e5775cf92d691be1769657d325a7d2abac
Nikhil Taneja <nikhil.a.taneja@gmail.com>
2013-12-10 18:17:01 -0700

    Add approve and show links for platform admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
 
   def viewable_restaurants
+    return Restaurant.all if current_user && current_user.platform_admin
+    
     Restaurant.where(display:true)
   end
 
