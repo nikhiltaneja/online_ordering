8fccf22781d9b6db20fd78a34ed363666457d51d
Nikhil Taneja <nikhil.a.taneja@gmail.com>
2013-12-11 21:56:00 -0700

    Add email notifications when restaurants are requested

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -15,6 +15,8 @@ class RestaurantsController < ApplicationController
   def create
     @restaurant = Restaurant.create!(restaurant_params)
     Role.create!(restaurant: @restaurant, user: current_user, level: "admin")
+    UserMailer.notify_restaurant_admin(current_user, @restaurant).deliver
+    UserMailer.notify_platform_admin(PlatformAdmin.first.user, @restaurant).deliver
     render :restaurant_confirmation
   end
 
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,18 @@
 class UserMailer < ActionMailer::Base
   default from: "denvergschool@gmail.com"
 
+  def notify_restaurant_admin(restaurant_admin, restaurant)
+    @restaurant_admin = restaurant_admin
+    @restaurant = restaurant
+    mail(:to => "#{restaurant_admin.email}", :subject => "Request received")
+  end
+
+  def notify_platform_admin(platform_admin, restaurant)
+    @restaurant = restaurant
+    @restaurant_admin = restaurant.roles.first.user
+    mail(:to => "#{platform_admin.email}", :subject => "Request for a new restaurant")
+  end
+
   def approved_confirmation(restaurant_admin, restaurant)
     @restaurant_admin = restaurant_admin
     @restaurant = restaurant
