8d9a8718b30193e78d8c956d9e25193cb6e6308c
QuillyT <QuillyT@gmail.com>
2013-12-17 13:54:01 -0700

    Improve security in user controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,15 +1,12 @@
 class CategoriesController < ApplicationController
   def index
-
     unless Restaurant.find_by(slug: params[:slug]).display
       flash.alert = "Restaurant isn't available."
       redirect_to root_path
     end
-
     roles = current_restaurant.roles.includes(:user)
     @admins = roles.where(level: "admin").map(&:user)
     @admins = roles.where(level: "stocker").map(&:user)
     @categories = current_restaurant.categories
   end
-
 end
