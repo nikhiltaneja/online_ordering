18c3133b676ff89353f703a5c116de9161492c8a
QuillyT <QuillyT@gmail.com>
2013-12-17 14:00:12 -0700

    Improve Admin orders query

--- app/controllers/customer_controller.rb
+++ app/controllers/customer_controller.rb
@@ -1,10 +1,6 @@
 class CustomerController < ApplicationController
 
   def index
-    @users = Order.where(restaurant: current_restaurant).each_with_object([]) do |order, users|
-      unless order.user_id.nil?  
-        users << User.find(order.user_id)
-      end
-    end
+    @users = Order.where(restaurant: current_restaurant).includes(:users).map(&:user)
   end
 end
